2080786ad9f9d56da6e36c42c9bb7577fe0754d0
Anurag Katiyar <anuragkatiyar1994@gmail.com>
2022-10-11 00:22:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector(".menu-icon");
+const mobileMenu= document.querySelector(".mobile-menu");
+
+
+menuIcon.addEventListener("click", ()=>{
+    mobileMenu.classList.toggle("show")
+})
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        verylightGreen: "#f3fcf4",
+        lightGreen: "#008060",
+        lightCream: "#fbf7ed",
+        darkGreen: "#004c3f",
+      },
+      fontFamily: 'Roboto', 
+      w50: '50%',
+    },
+  },
+  plugins: [],
+};
